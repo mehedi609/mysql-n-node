42e983c77c3d7294736187d96f1f01dd15455ad8
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2019-09-28 23:27:59 +0600

    "initial commit"

---
+++ README.md
@@ -0,0 +1,6 @@
+### It's a blueprint for setup Node.js with mysql, postgresql, sqlite etc. relational database and use Sequelize as ORM.
+
+#### Using this app you have to do the following work
+- Just clone the repo
+- Change the .env file according to your preference
+- You can also edit scripts in package.json file if you don't want to use devtools.
---
+++ models/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const config = require('./../config/config');
+const db = {};
+
+console.log(config);
+
+const { database, username, password, host } = config.db;
+
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'mysql'
+});
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = db;
